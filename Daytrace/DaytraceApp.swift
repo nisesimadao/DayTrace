5d3d662ebaa163c6c09b48a0fce1511b74c6b47e
Nisesimadao <121225551+nisesimadao@users.noreply.github.com>
2026-08-13 10:09:58 +0900

    Handle review reminder notification taps

--- Daytrace/DaytraceApp.swift
+++ Daytrace/DaytraceApp.swift
@@ -1,8 +1,36 @@
 import SwiftData
 import SwiftUI
+import UIKit
+import UserNotifications
+
+final class DaytraceAppDelegate: NSObject, UIApplicationDelegate, @preconcurrency UNUserNotificationCenterDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        UNUserNotificationCenter.current().delegate = self
+        return true
+    }
+
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        didReceive response: UNNotificationResponse,
+        withCompletionHandler completionHandler: @escaping () -> Void
+    ) {
+        let userInfo = response.notification.request.content.userInfo
+        if userInfo[ReviewReminderService.destinationKey] as? String == ReviewReminderService.todayDestination {
+            DispatchQueue.main.async {
+                NotificationCenter.default.post(name: .daytraceOpenToday, object: nil)
+            }
+        }
+        completionHandler()
+    }
+}
 
 @main
 struct DaytraceApp: App {
+    @UIApplicationDelegateAdaptor(DaytraceAppDelegate.self) private var appDelegate
+
     private let container: ModelContainer = {
         let schema = Schema([
             LocationEvidence.self,
